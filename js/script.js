be40fcb998a1452bfe2d3c19c1cd3b2c8807a29f
Robert <r.eriksson86@gmail.com>
2025-12-30 21:13:07 +0100

    added filter-meny dropdowns using JS

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+// Toggle filter options on button click.
+//takes all divs inside filter-meny
+const filters = document.querySelectorAll('.filter-meny > div');
+
+//adds event listener to each button inside the divs. toggle active class on click
+
+filters.forEach(filter => {
+    const button = filter.querySelector('button');
+    button.addEventListener('click', () => {
+        filter.classList.toggle('active');
+    });
+});
+
